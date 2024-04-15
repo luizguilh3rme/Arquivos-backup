46ce63870ae26d2b8f0c2dc21e54b1ba9608d841
luizguilh3rme <166665419+luizguilh3rme@users.noreply.github.com>
2024-04-15 18:59:47 -0300

    Add files via upload

---
+++ js_div/script.js
@@ -0,0 +1,93 @@
+//massas
+function clicar(){
+    var a = window.document.getElementById('container')
+    var b = window.document.getElementById('div-mostrar')
+    var c = window.document.getElementById('voltar')
+    c.style.display = 'block'
+    a.style.display = 'none'
+
+    if(a.style.display == 'none'){
+       b.style.display = 'block'
+    }
+
+    else{
+        a.style.display == 'none'
+    }
+       
+}
+
+function clicarM(){
+    var m = window.document.getElementById('div-mostrar')
+    var n = window.document.getElementById('mostrart')
+    var o = window.document.getElementById('voltar2')
+    o.style.display = 'block'
+    m.style.display = 'none'
+
+    if(m.style.display == 'none'){
+        n.style.display = 'block'
+    }
+
+    else{
+        m.style.display == 'none'
+    }
+}
+
+function voltar(){
+    var V = window.document.getElementById('div-mostrar')
+    var v = window.document.getElementById('container')
+    V.style.display = 'none'
+
+    if(V.style.display == 'none'){
+
+        v.style.display = 'block'
+    }
+
+    else{
+        V.style.display == 'none'
+    }
+}
+
+function voltar2(){
+    var V = window.document.getElementById('mostrart')
+    var v = window.document.getElementById('div-mostrar')
+    V.style.display = 'none'
+
+    if(V.style.display == 'none'){
+
+        v.style.display = 'block'
+    }
+
+    else{
+        V.style.display == 'none'
+    }
+}
+
+//bebidas
+function clicar2(){
+    var a = window.document.getElementById('container')
+    var b = window.document.getElementById('div-mostrar2')
+    a.style.display = 'none'
+
+    if(a.style.display == 'none'){
+       b.style.display = 'block'
+    }
+
+    else{
+        a.style.display == 'none'
+    }
+}
+
+function voltar3(){
+    var G = window.document.getElementById('div-mostrar2')
+    var g = window.document.getElementById('container')
+    G.style.display = 'none'
+
+    if(G.style.display == 'none'){
+
+        g.style.display = 'block'
+    }
+
+    else{
+        G.style.display == 'none'
+    }
+}
