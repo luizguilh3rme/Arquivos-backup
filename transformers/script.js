f4041960a2c5e4096d3a1a27a3feebe22db8aed5
luizguilh3rme <166665419+luizguilh3rme@users.noreply.github.com>
2024-04-15 19:01:50 -0300

    Add files via upload

---
+++ transformers/script.js
@@ -0,0 +1,22 @@
+function dece(){
+    document.getElementById("principal").style.display = 'none';
+    let mega = document.getElementById("mega").innerHTML = `<h2>Megatron<h2/>`;
+    document.getElementById("star").innerHTML = `<h2>StarScream<h2/>`;
+    document.getElementById("shock").innerHTML = `<h2>ShockWave<h2/>`;
+    document.getElementById("scourge").innerHTML = `<h2>Scourge<h2/>`;
+    document.getElementById("fallen").innerHTML = `<h2>Fallen<h2/>`;
+
+    mega.style.zIndex = "999";
+}
+
+function i1(){
+    document.getElementById("mega");
+    let vermelho = document.getElementById("vermelho");
+   let oi =  document.getElementById("img");
+    vermelho.style.zIndex = "999";
+    document.querySelector("body").innerText = `<h2>Megatron<h2/>`;
+    document.body.innerHTML = `<img src = '/decepticons/desktop-wallpaper-christopher-lee-zammit-transformers-the-last-knight-megatron.jpg'; />`;
+    body.oi.style.cssText = "background-repeat: no-repeat;background-size: cover;width: 100%;height: 100vh;"
+    // imagem.src = '/decepticons/desktop-wallpaper-christopher-lee-zammit-transformers-the-last-knight-megatron.jpg';
+    
+}
