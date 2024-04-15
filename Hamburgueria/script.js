be17d97c22991aea91ffb195c89342ae6aea23b5
luizguilh3rme <166665419+luizguilh3rme@users.noreply.github.com>
2024-04-15 18:59:17 -0300

    Add files via upload

---
+++ Hamburgueria/script.js
@@ -0,0 +1,142 @@
+function promocoes(){
+    var a = window.document.querySelector('.ai')
+    var b = window.document.querySelector('#mostrar')
+    var c = window.document.querySelector('#voltar')
+    c.style.display = 'block'
+    a.style.display = 'none'
+
+    if(a.style.display == 'none'){
+       b.style.display = 'block';
+    }
+
+    else{
+        a.style.display == 'none'
+    }
+       
+}
+
+function voltar(){
+    var V = window.document.querySelector('#mostrar')
+    var v = window.document.querySelector('.ai')
+    V.style.display = 'none'
+
+    if(V.style.display == 'none'){
+
+        v.style.display = 'block'
+    }
+
+    else{
+        V.style.display == 'none'
+    }
+}
+
+
+
+
+
+// function especial(){
+//     const q = window.document.querySelector('.ai')
+//     const w = window.document.querySelector('#mostrart')
+//     const e = window.document.querySelector('#voltar2')
+//     e.style.display = 'block'
+//     q.style.display = 'none'
+
+//     if(q.style.display == 'none'){
+//        w.style.display = 'block'
+//     }
+
+//     else{
+//         q.style.display == 'none'
+//     }
+       
+// }
+
+// function voltar2(){
+//     var T = window.document.querySelector('#mostrart')
+//     var t = window.document.querySelector('.ai')
+//     T.style.display = 'none'
+
+//     if(T.style.display == 'none'){
+
+//         t.style.display = 'block'
+//     }
+
+//     else{
+//         T.style.display == 'none'
+//     }
+// }
+
+
+ function drink(){
+    let drink1 = window.document.querySelector('.ai')
+    let drink2 = window.document.querySelector('#mostrar2')
+    let drink3 = window.document.querySelector('#voltar3')
+     drink3.style.display = 'block'
+     drink1.style.display = 'none'
+
+    if(drink1.style.display == 'none'){
+        drink2.style.display = 'block'
+     }
+
+    else{
+        drink1.style.display == 'none'
+     }
+       
+ }
+
+function voltar3(){
+     let L = window.document.querySelector('#mostrar2')
+     let l = window.document.querySelector('.ai')
+     L.style.display = 'none'
+
+     if(L.style.display == 'none'){
+
+         l.style.display = 'block'
+     }
+
+     else{
+         L.style.display == 'none'
+    }
+ }
+
+
+ function acompanhamento(){
+    let z = window.document.querySelector('.ai')
+    let x = window.document.querySelector('#mostrar3')
+    let n = window.document.querySelector('#voltar4')
+     n.style.display = 'block'
+     z.style.display = 'none'
+
+     if(z.style.display == 'none'){
+        x.style.display = 'block'
+     }
+
+     else{
+         z.style.display == 'none'
+     }
+       
+ }
+
+ function voltar4(){
+     let M = window.document.querySelector('#mostrar3')
+     let m = window.document.querySelector('.ai')
+     M.style.display = 'none'
+
+     if(M.style.display == 'none'){
+
+         m.style.display = 'block'
+     }
+
+     else{
+         M.style.display == 'none'
+     }
+ }
+
+
+ function alterar(){
+    let carrinho = document.getElementById("c1");
+    carrinho.innerText = 'Item adicionado';
+ }
+
+
+
